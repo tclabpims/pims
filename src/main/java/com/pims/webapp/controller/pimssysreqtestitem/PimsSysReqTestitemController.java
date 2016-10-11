66ce9e8de79f73dc448fb847f963499bb772b292
cqupgj <909436637@qq.com>
2016-10-11 09:31:34 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	pims.iml

--- src/main/java/com/pims/webapp/controller/pimssysreqtestitem/PimsSysReqTestitemController.java
+++ src/main/java/com/pims/webapp/controller/pimssysreqtestitem/PimsSysReqTestitemController.java
@@ -1,7 +1,6 @@
 package com.pims.webapp.controller.pimssysreqtestitem;
 
 
-import com.pims.model.PimsBaseModel;
 import com.pims.model.PimsSysReqTestitem;
 import com.pims.service.pimssysreqtestitem.PimsSysReqTestitemManager;
 import com.pims.webapp.controller.GridQuery;
@@ -9,6 +8,8 @@ import com.pims.webapp.controller.PIMSBaseController;
 import com.pims.webapp.controller.WebControllerUtil;
 import com.smart.webapp.util.DataResponse;
 import com.smart.webapp.util.PrintwriterUtil;
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -76,16 +77,23 @@ public class PimsSysReqTestitemController extends PIMSBaseController{
 
 	@RequestMapping(value = "/ajax/item*", method = RequestMethod.GET)
 	@ResponseBody
-	public DataResponse getTestitemInfo(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		DataResponse dataResponse = new DataResponse();
-		PimsBaseModel ppr = new PimsBaseModel(request);
-		List<PimsSysReqTestitem> list = pimsSysReqTestitemManager.getTestitemInfo();
-		if(list == null || list.size() == 0) {
-			return null;
+	public String getTestitemInfo(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String name = request.getParameter("name");
+//		if (StringUtils.isEmpty(name)) {
+//			return null;
+//		}
+		List<PimsSysReqTestitem> list = pimsSysReqTestitemManager.getTestitemInfo(name);
+		JSONArray array = new JSONArray();
+		if (list != null) {
+			for (PimsSysReqTestitem s : list) {
+				JSONObject o = new JSONObject();
+				o.put("id", s.getTestitemid());
+				o.put("name", s.getTeschinesename());
+				array.put(o);
+			}
 		}
-		dataResponse.setRecords(list.size());
-		dataResponse.setRows(getResultMap(list));
 		response.setContentType("text/html; charset=UTF-8");
-		return dataResponse;
+		response.getWriter().write(array.toString());
+		return null;
 	}
 }
